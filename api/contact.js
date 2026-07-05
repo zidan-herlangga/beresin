40c34d3ce9dd269ea2f27ac5ae100ee995751dac
Zidan Herlangga <zidantech04@gmail.com>
2026-07-05 15:11:40 +0700

    fix: update nodemailer import to dynamic import syntax

--- api/contact.js
+++ api/contact.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "Semua field wajib diisi" });
   }
 
-  const nodemailer = require("nodemailer");
+  const nodemailer = await import("nodemailer");
 
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
